b710b977594c71748013e44e0728e0c06a2526f1
Harsh Morayya <harshmorayya@ymail.com>
2024-10-28 16:42:44 +0530

    Modified: Replaced bootstrap header with daisyui header

--- client/src/components/layout/TheHeader.tsx
+++ client/src/components/layout/TheHeader.tsx
@@ -1,37 +1,80 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
-
 const HeaderNavLink = (props: { title: string; toHref: string }) => {
   const { title, toHref } = props;
 
   return (
-    <Nav.Link className="text-white" href={toHref}>
-      <span className="hover:text-primary font-plain text-sm">
-        {title.toUpperCase()}
-      </span>
-    </Nav.Link>
+    <div className="text-white">
+      <a href={toHref}>
+        <span className="hover:text-primary font-plain text-sm">
+          {title.toUpperCase()}
+        </span>
+      </a>
+    </div>
   );
 };
 
 const TheHeader = () => {
   return (
-    <Navbar bg="black" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand>
-          <span className="text-3xl font-bold text-white">Woxsen League</span>
-        </Navbar.Brand>
-        <Nav
-          className="ms-auto gap-x-12"
-          style={{ justifyContent: "flex-end" }}
-        >
-          <HeaderNavLink title="home" toHref="#" />
-          <HeaderNavLink title="about" toHref="#" />
-          <HeaderNavLink title="events" toHref="#" />
-          <HeaderNavLink title="facilities" toHref="#" />
-          <HeaderNavLink title="booking" toHref="#" />
-          <HeaderNavLink title="pro shop" toHref="#" />
-        </Nav>
-      </Container>
-    </Navbar>
+    <div className="navbar font-plain lg:px-72 bg-black text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm bg-black dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Something</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl lg:text-3xl font-extrabold">
+          Woxsen Sports Academy
+        </a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal gap-x-8 [&_a:hover]:text-primary px-1">
+          <li>
+            <a>HOME</a>
+          </li>
+          <li>
+            <a>ABOUT</a>
+          </li>
+          <li>
+            <a>EVENTS</a>
+          </li>
+          <li>
+            <a>FACILITIES</a>
+          </li>
+          <li>
+            <a>BOOKING</a>
+          </li>
+          <li>
+            <a>PRO SHOP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
