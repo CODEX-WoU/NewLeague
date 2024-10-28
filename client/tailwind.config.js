4ac00fbc193ce257c312d73fe1c025251c5d0cea
Harsh Morayya <harshmorayya@ymail.com>
2024-10-28 16:42:44 +0530

    Modified: Replaced bootstrap header with daisyui header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{tsx,jsx}"],
   theme: {
@@ -11,5 +13,8 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
 };
