fd4b61b6fa4b492b572353e3f7a8e745ebfdbb86
Rohith Kaki <rohithkaki11@gmail.com>
2024-10-29 15:49:42 +0530

    Changed app.tsx to default

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
-import EventCard from './components/EventCard'
-import yogaImage from './assets/yoga.jpg'
-import aerobicImage from './assets/aerobic.jpg'
-import cardioImage from './assets/cardio.jpg'
-import './App.css'
+import { Badge, Button } from "react-bootstrap";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
-const App: React.FC = () => {
-
+function App() {
   return (
     <>
-      <h2 className='text-lg text-gray-500 font-sans font-semibold'>Woxen Sports Academy</h2>
-      <h2 className="text-4xl font-bold font-sans text-center mb-6">Our Training Classes</h2>
-      <div className="flex flex-col md:flex-row justify-between">
-        <EventCard eventName="Yoga" trainedBy="Bella" info="Lorem ipsum dolor sit amet, consectetur adipiscing elit." price={50} image={yogaImage} />
-        <EventCard eventName="Cardio" trainedBy="Cathe" info="Lorem ipsum dolor sit amet, consectetur adipiscing elit." price={100} image={aerobicImage} />
-        <EventCard eventName="Aerobic" trainedBy="Mary" info="Lorem ipsum dolor sit amet, consectetur adipiscing elit." price={75} image={cardioImage} />
+      <div>
+        <h1>
+          Example heading
+          <Badge bg="primary" as={Button}>
+            Sample Badge to check bootstrap is working
+          </Badge>
+        </h1>
       </div>
+      <h1 className="text-4xl">Bootstrap header checker</h1>
+      <RouterProvider router={router} />
     </>
   );
-};
+}
 
 export default App;
