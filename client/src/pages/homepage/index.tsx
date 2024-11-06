891aff0d1b56a5851b2e30010bd595dc871678e4
Rohith Kaki <rohithkaki11@gmail.com>
2024-11-06 17:47:44 +0530

    Installed Daisy UI and added the About section

--- client/src/pages/homepage/index.tsx
+++ client/src/pages/homepage/index.tsx
@@ -1,6 +1,8 @@
 import EventSection from './EventSection'
+import About from './About'
 export const HomePage = () => {
   return <div>
+    <About/>
     <EventSection/>
   </div>
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{tsx,jsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import daisyui from "daisyui";
 
+export default {
+  content: ["./index.html", "./src/**/*.{tsx,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ef495d",
+      },
+      fontFamily: {
+        plain: ["Plain", "sans-serif"],
+      },
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
+};
