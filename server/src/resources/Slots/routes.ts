458f88cfee35535330603f91509b05dd3bc5e0df
Harsh Morayya <harshmorayya@ymail.com>
2025-01-16 15:10:25 +0530

    Added: Auth middlewares to slots' routes

--- server/src/resources/Slots/routes.ts
+++ server/src/resources/Slots/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { addMultipleSlotsController, addSlotController, updateSlotController } from "./controllers"
+import { verifyRoleMiddleware } from "../Auth/middleware"
 
 const slotsRouter = Router()
 
-slotsRouter.post("/", addSlotController)
-slotsRouter.post("/multiple", addMultipleSlotsController)
-slotsRouter.patch("/:id", updateSlotController)
+slotsRouter.post("/", verifyRoleMiddleware({ rolesAllowed: ["ADMIN"] }), addSlotController)
+slotsRouter.post("/multiple", verifyRoleMiddleware({ rolesAllowed: ["ADMIN"] }), addMultipleSlotsController)
+slotsRouter.patch("/:id", verifyRoleMiddleware({ rolesAllowed: ["ADMIN"] }), updateSlotController)
 
 export default slotsRouter
